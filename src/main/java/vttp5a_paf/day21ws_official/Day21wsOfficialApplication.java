efa584fa63c64e7db35f378251d37aa1d3a1fbac
Claaarree <clare.fh@hotmail.com>
2025-01-09 11:46:56 +0800

    initial commit

---
+++ src/main/java/vttp5a_paf/day21ws_official/Day21wsOfficialApplication.java
@@ -0,0 +1,13 @@
+package vttp5a_paf.day21ws_official;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21wsOfficialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21wsOfficialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp5a_paf/day21ws_official/Day21wsOfficialApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp5a_paf.day21ws_official;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21wsOfficialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
